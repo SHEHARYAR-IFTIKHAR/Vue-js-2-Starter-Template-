1922581a6c3d161d96367411d507cf12afaf6fbc
Sheharyar Iftikhar <sheharyar.isys@gmail.com>
2024-07-20 00:16:26 -0700

    vue js 2 starter template

---
+++ src/components/LayoutPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="w-full h-screen flex justify-center items-center border text-green-500 text-3xl"
+  >
+    Welcome to Vue js 2.
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  computed: {},
+  mounted() {},
+  watch: {},
+};
+</script>
+<style scoped></style>
